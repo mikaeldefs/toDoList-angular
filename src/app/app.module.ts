74736f05f0ca266277a727c0ed97984e7d0d995b
mikaeldefs <mikaeldefreitas@hotmail.com>
2021-08-30 21:49:15 -0300

    Adicionado os componentes Tarefa, Lista de Tarefas e Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarefaComponent } from './components/tarefa/tarefa.component';
+import { ListaDeTarefasComponent } from './components/lista-de-tarefas/lista-de-tarefas.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarefaComponent,
+    ListaDeTarefasComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
